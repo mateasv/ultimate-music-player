1c766095d81df313eb008f76112e5174855087f7
a <a>
2021-01-06 02:04:26 +0100

    Added player.fxml UI, and a simple temporary controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    ObservableList<String> items = FXCollections.observableArrayList (
+            "Test Song 1", "Test Song 2", "Test Song 3", "Test Song 4");
+
+    @FXML
+    ListView<String> songsList = new ListView<String>(items);
+
+    @FXML
+    TextField textFieldSearch = new TextField();
+
+    @FXML
+    Button playBtn = new Button();
+
+
+
+    @FXML
+    public void playBtnClicked(Event e) {
+        playBtn.setText(playBtn.getText().equals("PLAY") ? "PAUSE" : "PLAY");
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        songsList.setItems(items);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 // Test commit
 
@@ -12,12 +18,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("player.fxml"));
+        primaryStage.setTitle("Ultimate Music Player");
+        primaryStage.setScene(new Scene(root, 450, 350));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
