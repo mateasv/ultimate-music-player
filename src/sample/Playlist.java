2dbfb5db51bbd369cf3c1efff20d7cd4680c2d90
a <a>
2021-01-06 14:19:11 +0100

    Added JavaDocs comments, cleaned some code, small changes

--- src/sample/Playlist.java
+++ src/sample/Playlist.java
@@ -11,8 +11,6 @@ public class Playlist  {
     public Playlist(String playlistName, ArrayList<Song> playlist) {
         this.playlistName = playlistName;
         this.playlist = playlist;
-
-
     }
 
 
@@ -24,32 +22,31 @@ public class Playlist  {
 //
 //    }
 
-    //create song
-    public Song createSong(String songTitle, String artistName, String songGenre, String filePath){
-        Song song = new Song(songTitle, artistName, songGenre, filePath);
-        return song;
+    /**
+     * Method to append a song to the end of this playlist
+     * on pressed button add song to the end of the arraylist
+     */
+    public void addToPlaylist(Song song){
+        playlist.add(song);
     }
 
-    //method to add songs to a playlist
-    //on pressed button add song to the end of the arraylist
-    public void addToPlaylist(){
-        playlist.add(createSong("baby", "kris", "rock", "songs"));
+    /**
+     * Method to remove songs from a playlist
+     * on pressed button, get the arraylist number of of the song and remove it.
+     *
+     * @param index Index of the song to be removed (0 = first song in the playlist)
+     */
+    public void removeFromPlaylist(int index){
+        playlist.remove(index);
 
     }
 
-    //method to remove songs from a playlist
-    //on pressed button, get the arraylist number of of the song and remove it.
-    public void removeFromPlaylist(){
-        playlist.remove(1);
-
-    }
-
-    //method to delete a playlist
-    //on pressed button, delete the contents of the chosen arraylist.
-    //delete the chosen arraylist from the list of playlists
-    public  void deletePlaylist(){
+    /**
+     * Method to delete a playlist
+     * on pressed button, delete the contents of the chosen arraylist.
+     * Delete the chosen arraylist from the list of playlists
+     */
+    public void deletePlaylist(){
         playlist.clear();
-
-
     }
 }
