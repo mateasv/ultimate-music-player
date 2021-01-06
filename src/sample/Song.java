e8c875c68593b60ef61c27fdb0591e3b839ea8a8
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2021-01-06 11:03:42 +0100

    Song Updated Kristoffer Branch

--- src/sample/Song.java
+++ src/sample/Song.java
@@ -5,14 +5,38 @@ import java.io.*;
 
 public class Song {
 
+    private String title, artist, genre, path;
+    private File song;
+
 
     private Song(String songTitle, String artistName, String songGenre, String filePath){
-
+            this.title = songTitle;
+            this.artist = artistName;
+            this.genre = songGenre;
+            this.path = filePath;
     }
 
     //Find song metadata
     public void getSongData(){
-
+        song = new File(System.getProperty("user.dir"));
 
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+
 }
