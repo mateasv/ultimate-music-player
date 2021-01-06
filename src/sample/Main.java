218ad2955490b7ce3fe42e2d2c2a16985ddbe39c
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2021-01-06 10:10:05 +0100

    Second Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// Test commit
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -19,7 +19,40 @@ public class Main extends Application {
     }
 
 
+    static File dir = new File(System.getProperty("user.dir"));
+
     public static void main(String[] args) {
+
+        createFolder(hasSongsFolder(dir), dir);
+
         launch(args);
+    }
+
+    public static boolean hasSongsFolder(File directory) {
+        String[] contents = directory.list();
+        assert contents != null;
+        for(String content: contents){
+            if(content.equals("songs")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void createFolder(boolean exists, File directory){
+        String path = "\\songs";
+        boolean created;
+        if(exists){
+            System.out.println("Folder already exists");
+            return;
+        }
+            directory = new File(directory + path);
+            created = directory.mkdir();
+            if(created){
+                System.out.println("The folder has been created");
+            }
+            else{
+                System.out.println("The folder could not be created");
+            }
     }
 }
---
+++ src/sample/Song.java
@@ -0,0 +1,18 @@
+package sample;
+
+
+import java.io.*;
+
+public class Song {
+
+
+    private Song(String songTitle, String artistName, String songGenre, String filePath){
+
+    }
+
+    //Find song metadata
+    public void getSongData(){
+
+
+    }
+}
